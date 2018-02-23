c9561668d6d16933d467e4a0625a8e7c641be9ef
Denys Smirnov <denys@sourced.tech>
2018-02-23 16:48:58 +0100

    update to SDK v1.13.2
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/main.go
+++ driver/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"github.com/bblfsh/cpp-driver/driver/normalizer"
 
-	"github.com/bblfsh/sdk"
-
-	_ "github.com/bblfsh/java-driver/driver/normalizer"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver"
 )
 
-var version string
-var build string
-
 func main() {
-	fmt.Printf("version: %s\nbuild: %s\n", version, build)
-
-	_, err := os.Stat(sdk.NativeBin)
-	if err == nil {
-		fmt.Println("native: ok")
-		return
-	}
-
-	fmt.Printf("native: %s\n", err)
+	driver.Run(normalizer.ToNode, normalizer.Transformers)
 }
